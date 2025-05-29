094e7a635c119889b52fb0d72247296467dda303
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-05-29 22:15:45 +0530

    feat: design ui for the tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,130 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { ImageBackground, Image, Text, View, StyleSheet } from 'react-native';
+import { images } from '@/constants/images';
+import { icons } from '@/constants/icons';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+type TabIconProps = {
+    focused: boolean;
+    icon: any;
+    title: string;
+};
+
+const TabIcon = ({ focused, icon, title }: TabIconProps) => {
+    return focused ? (
+        <ImageBackground
+            source={images.highlight}
+            style={styles.activeTabBackground}
+            resizeMode="contain"
+        >
+            <View style={styles.activeTabContent}>
+                <Image
+                    source={icon}
+                    style={[styles.icon, { tintColor: '#151312' }]}
+                />
+                <Text style={styles.activeTabText}>{title}</Text>
+            </View>
+        </ImageBackground>
+    ) : (
+        <View style={styles.inactiveTab}>
+            <Image
+                source={icon}
+                style={[styles.icon, { tintColor: '#A8B5DB' }]}
+            />
+        </View>
+    );
+};
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarStyle: styles.tabBar,
+                tabBarItemStyle: styles.tabBarItem,
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon focused={focused} icon={icons.home} title="Home"/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: 'Search',
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon focused={focused} icon={icons.search} title="Search"/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="saved"
+                options={{
+                    title: 'Saved',
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon focused={focused} icon={icons.save} title="Saved"/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon focused={focused} icon={icons.person} title="Profile"/>
+                    )
+                }}
+            />
+        </Tabs>
+    );
 }
+
+const styles = StyleSheet.create({
+    tabBar: {
+        height: 70,
+        backgroundColor: '#0F0D23',
+        borderTopWidth: 0,
+        elevation: 200,
+        shadowOpacity: 0,
+        borderRadius: 100
+    },
+    tabBarItem: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    activeTabBackground: {
+        width: 112,
+        height: 56,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    activeTabContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 12,
+    },
+    activeTabText: {
+        color: '#151312',
+        fontSize: 14,
+        fontWeight: '600',
+        marginLeft: 8,
+    },
+    inactiveTab: {
+        width: 40,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    icon: {
+        width: 20,
+        height: 20,
+    },
+});
---
+++ app/movies/[id].tsx
@@ -0,0 +1,8 @@
+import { Text, View } from 'react-native';
+export default function MovieDetails(){
+    return(
+        <View>
+            <Text>Movie Details</Text>
+        </View>
+    )
+}
