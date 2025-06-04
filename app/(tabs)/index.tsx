f594e6d839946d1c286edd7c0371a74e030289e6
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-06-05 00:21:23 +0530

    add search functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,22 @@
 import {Image, Text, View, SafeAreaView, ScrollView} from 'react-native';
 import { images } from '@/constants/images';
 import { icons } from '@/constants/icons';
+import SearchBar from '@/components/SearchBar'
+import {useRouter} from "expo-router";
 
 export default function HomeScreen() {
+    const router = useRouter();
+
     return (
         <>
         <View className={"flex-1 bg-primary"}>
             <Image source={images.bg} className={"absolute z-0 w-full"}/>
             <ScrollView className={"flex-1 px-5"} showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
                 <Image source={icons.logo} className={"w-12 h-10 mt-20 mx-auto mb-5"}/>
+                <SearchBar
+                    onPress={() => router.push("/search")}
+                    placeholder={"Search a movie"}
+                />
             </ScrollView>
         </View>
         </>
---
+++ components/SearchBar.tsx
@@ -0,0 +1,19 @@
+import {View, TextInput, Image} from 'react-native';
+import React from 'react'
+import {icons} from '@/constants/icons'
+
+interface Props {
+    placeholder: string
+    onPress?: () => void
+}
+
+const SearchBar = ({placeholder,onPress}:Props) => {
+    return (
+        <View className={"flex-row items-center bg-dark-200 rounded-full px-5 py-4"}>
+            <Image source={icons.search} className={"text-white"} resizeMode="contain" tintColor={"#ab8bff"} />
+            <TextInput onPress={onPress} placeholder={placeholder} value={""} onChangeText={() => {}} placeholderTextColor={"#a8b5db"}  className={"flex ml-2 text-white"} />
+        </View>
+    )
+}
+
+export default SearchBar;
